647d8cc06358491d8cfd8eeb0961d2ce5ffb8c28
7vik-1403 <satvikdeshmukh24@gmail.com>
2026-07-14 21:10:25 +0530

    made changes deleted and created whole db

--- alembic/env.py
+++ alembic/env.py
@@ -2,17 +2,17 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
-from alembic import context
 from app.core.config import settings
+from alembic import context
 from app.db.database import Base
-from app.models.students_profile import StudentProfile 
-
+import app.models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url",settings.database_url)
-
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.database_url
+)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -23,7 +23,6 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
@@ -62,10 +61,10 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    config.get_section(config.config_ini_section, {}),
+    prefix="sqlalchemy.",
+    poolclass=pool.NullPool,
+)
 
     with connectable.connect() as connection:
         context.configure(
